f644b82c4e1f77a5fcacc81990f9f164ad5717a5
Jordan <104177490+J-R2@users.noreply.github.com>
2024-09-26 14:58:58 -0500

    added player movement

--- scenes/player_scene/player.gd
+++ scenes/player_scene/player.gd
@@ -1,28 +1,32 @@
+class_name Player
 extends CharacterBody2D
 
+const SPEED :int = 200
+const STEERING_FORCE :int = 8
+const SPRITE_SIZE :int = 90 / 2
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+@onready var screen_size = get_viewport_rect().size
+var direction := Vector2.ZERO
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _ready() -> void:
+	pass
 
+
+func _process(delta: float) -> void:
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	direction = direction.normalized()
+	var desired_velocity :Vector2 = direction * SPEED
+	var steering_vector :Vector2 = desired_velocity - velocity
+	velocity += steering_vector * STEERING_FORCE * delta
+	if desired_velocity.length() > 0.1:
+		rotation = velocity.angle()
+		animated_sprite_2d.play("move")
+	else: animated_sprite_2d.stop()
 	move_and_slide()
+	position.x = wrapf(position.x, -SPRITE_SIZE, screen_size.x + SPRITE_SIZE)
+	position.y = wrapf(position.y, -SPRITE_SIZE, screen_size.y + SPRITE_SIZE)
